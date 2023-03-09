ef46b32769ea6383929d3d2a253ef1ebcf0e3d4d
Braden Hilton <hiltonbraden@gmail.com>
2023-03-09 07:38:43 +0000

    feat: Allow custom format

--- yt_dlp_plugins/postprocessor/fixup_mtime.py
+++ yt_dlp_plugins/postprocessor/fixup_mtime.py
@@ -1,44 +1,51 @@
+import contextlib
 import datetime
 import os
+import re
+
+
 from yt_dlp.postprocessor.common import PostProcessor
-from yt_dlp.utils import traverse_obj, datetime_from_str, replace_extension
+from yt_dlp.utils import date_formats, replace_extension
 
 
 class FixupMtimePP(PostProcessor):
-    def __init__(self, downloader=None, mtime_key=None):
+    def __init__(self, downloader=None, mtime_key='mtime', format='%Y%m%d'):
         super().__init__(downloader)
         self._mtime_key = mtime_key
+        self._format = re.sub(r'%%', '%', format)
+
+    def _strptime_or_none(self, timestamp, format, default=None):
+        datetime_object = None
+        try:
+            if isinstance(timestamp, (int, float)):
+                datetime_object = datetime.datetime.fromtimestamp(
+                    timestamp, datetime.timezone.utc)
+            elif isinstance(timestamp, str):
+                formats = [format]
+                formats.extend(date_formats(day_first=True))
+                for expression in formats:
+                    with contextlib.suppress(ValueError):
+                        datetime_object = datetime.datetime.strptime(
+                            timestamp, expression)
+            return datetime_object
+        except (ValueError, TypeError, AttributeError):
+            return default
 
     def run(self, info):
         filepath = info.get('filepath')
-
         if filepath:
-            files = {
-                filepath,
-                info.get('__infojson_filename'),
-            }
-
-            if self.get_param('keepvideo', False):
-                for format in traverse_obj(info, ('requested_formats', ...)) or []:
-                    format_id = format.get('format_id')
-                    format_ext = format.get('ext')
-
-                    if format_id and format_ext:
-                        files.add(replace_extension(
-                            filepath, f'f{format_id}.{format_ext}'))
-
+            files = {filepath, info.get('__infojson_filename')}
             for path in info.get('__files_to_move'):
                 files.add(path)
-
             if self.get_param('writedescription', False):
                 files.add(replace_extension(filepath, 'description'))
-
-            mtime = datetime.datetime.fromtimestamp(os.stat(
-                filepath).st_mtime, datetime.timezone.utc) if self._mtime_key == 'mtime' else datetime_from_str(info.get(self._mtime_key))
-
+            mtime = self._strptime_or_none(os.stat(
+                filepath).st_mtime if self._mtime_key == 'mtime'else info.get(self._mtime_key), self._format)
             if mtime:
                 self.to_screen(
                     f'Setting mtime of files to {mtime.isoformat()}')
                 for file in files:
                     self.try_utime(file, mtime.timestamp(), mtime.timestamp())
+            else:
+                self.to_screen('Unable to set mtime')
         return [], info
