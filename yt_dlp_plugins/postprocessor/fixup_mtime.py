1b5a87c7a67b340339fae575cc2bf3d636b93f7f
Braden Hilton <hiltonbraden@gmail.com>
2025-02-14 17:11:05 +0000

    chore: Switch to uv

--- yt_dlp_plugins/postprocessor/fixup_mtime.py
+++ yt_dlp_plugins/postprocessor/fixup_mtime.py
@@ -1,58 +1,57 @@
-import contextlib
-import datetime
+from __future__ import annotations
+
 import glob
 import os
 import re
-
-
+from contextlib import suppress
+from datetime import datetime, timezone
 from pathlib import Path
-from typing import Iterable, List, Optional, Union
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 from yt_dlp.postprocessor.common import PostProcessor
 from yt_dlp.utils import date_formats
 
 
 class FixupMtimePP(PostProcessor):
-    def __init__(self, downloader=None, mtime_key: str = 'mtime', mtime_format: str = '%Y%m%d') -> None:
+    def __init__(self, downloader=None, mtime_key: str = "mtime", mtime_format: str = "%Y%m%d") -> None:
         super().__init__(downloader)
         self._mtime_key = mtime_key
-        self._mtime_format = re.sub(r'%%', '%', mtime_format)
+        self._mtime_format = re.sub(r"%%", "%", mtime_format)
 
-    def _strptime_or_none(self, timestamp: Union[float, int, str, None], format_code: str) -> Optional[datetime.datetime]:
+    def _strptime_or_none(self, timestamp: float | str | None, format_code: str) -> datetime | None:
         if timestamp is None:
             return None
         if isinstance(timestamp, (float, int)):
-            return datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc)
-        for code in [format_code] + date_formats(day_first=True):
-            with contextlib.suppress(ValueError):
-                return datetime.datetime.strptime(timestamp, code)
+            return datetime.fromtimestamp(timestamp, timezone.utc)
+        for code in [format_code, *date_formats(day_first=True)]:
+            with suppress(ValueError):
+                return datetime.strptime(timestamp, code)  # noqa: DTZ007
         return None
 
-    def _get_related_files(self, filepath: Union[Path, str]) -> List[Path]:
+    def _get_related_files(self, filepath: Path | str) -> list[Path]:
         if isinstance(filepath, str):
             filepath = Path(filepath)
-        return [Path(path) for path in filepath.parent.glob(f'{glob.escape(filepath.stem)}.*')
-                if path.is_file()]
+        return [Path(path) for path in filepath.parent.glob(f"{glob.escape(filepath.stem)}.*") if path.is_file()]
 
-    def _get_mtime(self, filepath: Path, info: dict, mtime_key: str) -> Optional[Union[float, int, str]]:
-        return os.path.getmtime(filepath) if mtime_key == 'mtime' else info.get(mtime_key)
+    def _get_mtime(self, filepath: Path, info: dict, mtime_key: str) -> float | int | str | None:
+        return os.path.getmtime(filepath) if mtime_key == "mtime" else info.get(mtime_key)
 
-    def _set_mtime_of_files(self, files: Iterable[Path], mtime: Union[datetime.datetime, float, int]) -> None:
-        if isinstance(mtime, datetime.datetime):
+    def _set_mtime_of_files(self, files: Iterable[Path], mtime: datetime | float) -> None:
+        if isinstance(mtime, datetime):
             mtime = mtime.timestamp()
         for path in files:
             self.try_utime(str(path), os.path.getatime(path), mtime)
 
     def run(self, info: dict):
-        filepath = info.get('filepath')
+        filepath = info.get("filepath")
         if filepath:
-            mtime = self._strptime_or_none(self._get_mtime(
-                filepath, info, self._mtime_key), self._mtime_format)
+            mtime = self._strptime_or_none(self._get_mtime(filepath, info, self._mtime_key), self._mtime_format)
             if mtime:
-                self.to_screen(
-                    f'Setting mtime of files to `{self._mtime_key}` ({mtime.isoformat()})')
+                self.to_screen(f"Setting mtime of files to `{self._mtime_key}` ({mtime.isoformat()})")
                 self._set_mtime_of_files(self._get_related_files(filepath), mtime)
             else:
-                self.to_screen(f'Unable to set mtime to `{self._mtime_key}`')
+                self.to_screen(f"Unable to set mtime to `{self._mtime_key}`")
         return [], info
